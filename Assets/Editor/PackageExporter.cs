93607ef8e27d5430f39071eafb840d73cf5de96a
kyubuns <kyubuns@gmail.com>
2021-01-31 13:13:22 +0900

    feat: PackageExporter

---
+++ Assets/Editor/PackageExporter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Development
+{
+    public static class PackageExporter
+    {
+        private const string Target = "Assets/Auto9Slicer";
+
+        [MenuItem("Export/Package")]
+        public static void Export()
+        {
+            var packageText = AssetDatabase.LoadAssetAtPath<TextAsset>(Path.Combine(Target, "package.json"));
+            var package = JsonUtility.FromJson<PackageJson>(packageText.text);
+
+            var outputPath = Path.Combine(Path.GetDirectoryName(Application.dataPath) ?? "", $"{package.displayName}_v{package.version}.unitypackage");
+            AssetDatabase.ExportPackage(new[] { Target }, outputPath, ExportPackageOptions.Recurse);
+            Debug.LogFormat("ExportPackage {0}", outputPath);
+        }
+    }
+
+    [Serializable]
+    public class PackageJson
+    {
+        public string displayName;
+        public string version;
+    }
+}
