ec2e3d0da8924da190e0d9bca99fd2971eede310
kyubuns <kyubuns@gmail.com>
2021-02-06 01:37:35 +0900

    feat: UnityPackage作るときもDemoシーン入れる

--- Assets/Editor/PackageExporter.cs
+++ Assets/Editor/PackageExporter.cs
@@ -15,6 +15,11 @@ namespace Development
             var packageText = AssetDatabase.LoadAssetAtPath<TextAsset>(Path.Combine(Target, "package.json"));
             var package = JsonUtility.FromJson<PackageJson>(packageText.text);
 
+            var basePath = Path.Combine(Application.dataPath, "Auto9Slicer");
+            Directory.Move(Path.Combine(basePath, "Samples~/Demo"), Path.Combine(basePath, "Demo"));
+            File.Move(Path.Combine(basePath, "Samples~/Demo.meta"), Path.Combine(basePath, "Demo.meta"));
+            AssetDatabase.Refresh(ImportAssetOptions.Default);
+
             var outputPath = Path.Combine(Path.GetDirectoryName(Application.dataPath) ?? "", $"{package.displayName}_v{package.version}.unitypackage");
             AssetDatabase.ExportPackage(new[] { Target }, outputPath, ExportPackageOptions.Recurse);
             Debug.LogFormat("ExportPackage {0}", outputPath);
