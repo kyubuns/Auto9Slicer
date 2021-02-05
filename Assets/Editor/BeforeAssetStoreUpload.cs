38c024db016fd7196467d6445fb714bfa34c4b56
kyubuns <kyubuns@gmail.com>
2021-02-05 19:41:07 +0900

    feat: BeforeAssetStoreUploadScript

---
+++ Assets/Editor/BeforeAssetStoreUpload.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Development
+{
+    public static class BeforeAssetStoreUpload
+    {
+        private const string Target = "Assets/Auto9Slicer";
+
+        [MenuItem("Asset Store Tools/**Before Upload**")]
+        public static void Run()
+        {
+            Debug.Log("Start");
+            AssetDatabase.DeleteAsset("Assets/Auto9Slicer/LICENSE.md");
+            AssetDatabase.DeleteAsset("Assets/Auto9Slicer/README.md");
+            AssetDatabase.DeleteAsset("Assets/Auto9Slicer/package.json");
+            Debug.Log("Finish");
+        }
+    }
+}
